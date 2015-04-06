42135d27b9f12b105001aeea83b7ee3dcdbb234a
Patrick Wu <scorpio7500@gmail.com>
2015-04-06 12:03:33 -0400

    initial commit

---
+++ triangle.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author patrickwu
+ * 
+ */
+
+public class Triangle {
+	public static void main(Strings args[]) {
+		int tempMax, sum = 0;
+		Scanner sc = new Scanner(new File("triangle.txt"));
+
+		while (sc.hasNextLine()) {
+		// reads through each line, loops over each value and compares to temp max
+		}
+	}
+}
