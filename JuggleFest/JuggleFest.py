cbf6445c0673bde8911dd0b579a754ce5545845a
Patrick Wu <scorpio7500@gmail.com>
2015-12-07 06:00:25 +0000

    modularized classes

--- JuggleFest/JuggleFest.py
+++ JuggleFest/JuggleFest.py
@@ -2,37 +2,10 @@
 # author @Patrick Wu
 
 import sys, numpy
+from Juggler import Juggler
+from Circuit import Circuit
 from collections import OrderedDict
 
-class Juggler:
-	def __init__(self, H, E, P, name):
-		self.name = name
-		self.HEP = [H, E, P]
-
-	def __repr__(self):
-		return self.name + " H:" + str(self.HEP[0]) + " E:" + str(self.HEP[1]) + " P:" + str(self.HEP[2]) + " | Preferences:" + str(self.preferences)
-
-	def addPreferences(self,preferences):
-		self.preferences = preferences
-
-	def dotProduct(self, circuit, circuits):
-		dotProduct = 0
-		
-		for c in circuits:
-			if c.name == circuit:
-				found = c
-
-		for i in range(len(self.HEP)):
-			dotProduct += int(self.HEP[i]) * int(found.HEP[i])
-		return dotProduct
-
-class Circuit:
-	def __init__(self, H, E, P, name):
-		self.name = name
-		self.HEP = [H, E, P]
-
-	def __repr__(self):
-		return self.name + " H:" + str(self.HEP[0]) + " E:" + str(self.HEP[1]) + " P:" + str(self.HEP[2])
 
 def main():
 	fs = open('juggletest.txt')
